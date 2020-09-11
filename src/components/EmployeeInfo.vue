f517801e968c0b3320e511c731ff0e3d83a0ec0d
Karthick <karthickps.ai@gmail.com>
2020-09-11 20:09:13 +0530

    select and add employee button added

---
+++ src/components/EmployeeInfo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-container fluid>
+      <div class="text-center">
+        <v-btn class="mx-8" depressed large color="success">Select Employee</v-btn>
+        <v-btn class="mx-8" depressed large color="primary">Add Employee</v-btn>
+      </div>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <Header></Header>
+    <EmployeeInfo></EmployeeInfo>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from "@/components/Header.vue";
+import EmployeeInfo from "@/components/EmployeeInfo.vue";
 export default {
   name: "Home",
   components: {
     Header,
+    EmployeeInfo,
   },
 };
 </script>
