371b5962517ce029a80527cc4ee8ce304b3a7af7
Karthick <karthickps.ai@gmail.com>
2020-09-11 19:23:25 +0530

    Header View done

---
+++ src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <v-app-bar color="grey lighten-5">
+      <v-img
+        class="mx-2"
+        src="https://humetis.com/library/assets/images/Humetis_Logo.png"
+        max-height="50"
+        max-width="50"
+        contain
+      ></v-img>
+      <v-toolbar-title>Humetis Group</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item @click="() => {}">
+            <v-list-item-title>Change Password</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="() => {}">
+            <v-list-item-title>Logout</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+    <v-container fluid>
+      <v-row justify="center">
+        <!-- <v-col align="right">
+          <v-img
+            src="https://humetis.com/library/assets/images/Humetis_Logo.png"
+            height="50"
+            width="70"
+          ></v-img>
+        </v-col>-->
+        <v-col align="center">
+          <div class="text-h5">HuMetis Group</div>
+          <div class="text-h7">We are Transforming</div>
+          <div class="text-h7">From Consultancy to Company & From Consultants to Employees</div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
-  <div></div>
+  <div>
+    <Header></Header>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import Header from "@/components/Header.vue";
 export default {
   name: "Home",
-  components: {},
+  components: {
+    Header,
+  },
 };
 </script>
